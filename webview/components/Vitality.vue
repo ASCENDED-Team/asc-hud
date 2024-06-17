f790d24fd22468964de258f8821ee31f38ae1d5a
Liam Robinson <liamrobinson5@web.de>
2024-06-17 13:45:40 +0200

    Vitals getting their own Components

--- webview/components/Vitality.vue
+++ webview/components/Vitality.vue
@@ -1,41 +1,13 @@
 <template>
     <div class="vitality-container ml-2 flex flex-col gap-2">
-        <div class="health-wrapper vitals relative">
-            <div
-                class="vitality-health absolute left-[7px] flex h-full w-full items-center justify-center bg-gradient-to-r from-gray-500/70 to-transparent"
-            >
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path
-                        fill="white"
-                        d="m12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53z"
-                    />
-                </svg>
-            </div>
-            <div
-                class="vitality-health absolute bottom-0 left-0 h-full w-[5px] bg-red-500"
-                :style="{ height: `${health / 2}%` }"
-            ></div>
-        </div>
-        <div v-if="armour > 0" class="armour-wrapper vitals relative">
-            <div
-                class="vitality-shield absolute left-[7px] flex h-full w-full items-center justify-center bg-gradient-to-r from-gray-500/70 to-transparent"
-            >
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path
-                        fill="white"
-                        d="M12 22q-3.475-.875-5.738-3.988T4 11.1V5l8-3l8 3v6.1q0 3.8-2.262 6.913T12 22"
-                    />
-                </svg>
-            </div>
-            <div
-                class="vitality-health absolute bottom-0 left-0 h-auto w-[5px] bg-blue-500"
-                :style="{ height: `${armour}%` }"
-            ></div>
-        </div>
+        <Health :health="health" />
+        <Armour v-if="armour > 0" :armour="armour" />
     </div>
 </template>
 
 <script setup>
+import Health from './vitals/health.vue';
+import Armour from './vitals/armour.vue';
 const { health, armour } = defineProps(['health', 'armour']);
 </script>
 
